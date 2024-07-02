caf9775530e2e90985e2ed2681ab4e31753a1ecf
zxx <1206294197@qq.com>
2024-07-02 19:25:54 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//创建"外壳"组件App
+import React from "react";
+import Hello from "./components/Hello/Hello";
+import Welcome from "./components/Welcome/Welcome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//创建并暴露App组件
+export default class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Hello/>
+        <Welcome/>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,10 @@
+//引入
+import React,{Component} from "react";
+import './Hello.css'
+
+
+export default class Hello extends Component{
+    render(){
+        return <h2 className="title">hello react me</h2>
+    }
+}
